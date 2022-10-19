8ea2c1f51ec7c5e4a0af93950a849248fc66e738
Anderson <44785864+Atonalism@users.noreply.github.com>
2022-10-18 22:34:33 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 
 int main()
 {
-    Tree tree(0);
-    tree.backtracking(30);
+    Tree backtracking(0);
+    backtracking.backtracking(25);
+    Tree busca_em_largura(0);
+    busca_em_largura.bfs();
     return 0;
 }
