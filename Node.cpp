6845d2af8e48fcf888468c075b355d2cc93f0825
Anderson <44785864+Atonalism@users.noreply.github.com>
2022-10-24 08:28:54 -0300

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -10,14 +10,18 @@ Node::Node(std::vector<size_t> &state, std::vector<size_t> &pos, unsigned r, Nod
     this->positions.reserve(4);
     this->positions = pos;
 
+    // inicia todos os filhos como nulo
     this->children.reserve(4);
     for (size_t i = 0; i < 4; ++i)
         children[i] = NULL;
         
     this->parent = p;
 
-    for (size_t i = 0; i < 4; ++i)
-        R.push(i);
+    // preenche as ordens das regras
+    for (size_t i = 0; i < 4; ++i)       // da pra melhorar
+        fila_de_regras.push(i);
+    for (size_t i = 4; i > 0; --i)       // da pra melhorar
+        pilha_de_regras.push(i);
 }
 
 Node::~Node() {}
@@ -35,15 +39,10 @@ void Node::set_children(Node *p, size_t r) { children[r] = p; }
 auto Node::get_state() -> std::vector<size_t> { return state; }
 auto Node::get_positions() -> std::vector<size_t> { return positions; }
 
-auto Node::queue_front() -> size_t
-{
-    return R.front();
-}
-auto Node::queue_pop() -> void
-{
-    R.pop();
-}
-auto Node::queue_empty() -> bool
-{
-    return R.empty();
-}
+auto Node::queue_front() -> size_t { return fila_de_regras.front(); }
+auto Node::queue_pop() -> void { fila_de_regras.pop(); }
+auto Node::queue_empty() -> bool { return fila_de_regras.empty(); }
+
+auto Node::stack_front() -> size_t { return pilha_de_regras.top(); }
+auto Node::stack_pop() -> void { pilha_de_regras.pop(); }
+auto Node::stack_empty() -> bool { return pilha_de_regras.empty(); }
